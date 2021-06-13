eed000c87a3c72c927e5b1a4bf97ad09e58853bf
nancyshenouda <Shenouda.nancy@gmail.com>
2021-06-13 14:29:55 -0400

    creating hiding package / Phone class

---
+++ src/day53_inheritance/hiding/Phone.java
@@ -0,0 +1,9 @@
+package day53_inheritance.hiding;
+
+public class Phone {
+    String type = "Old school phone";
+
+    public static void use(){
+        System.out.println("Using the Phone.");
+    }
+}
---
+++ src/day53_inheritance/tesla/ModelX.java
@@ -0,0 +1,9 @@
+package day53_inheritance.tesla;
+
+public class ModelX extends ElectricCar{
+
+    public ModelX(String make, String model, double price, int year, int range) {
+        super(make, model, price, year, range);
+        System.out.println("Welcome new Model X");
+    }
+}
