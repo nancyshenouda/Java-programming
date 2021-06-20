b0af77d9511d652efaf0fbd98fda4dab73c757cf
nancyshenouda <Shenouda.nancy@gmail.com>
2021-06-20 12:39:00 -0400

    FreeWeight class added

---
+++ src/day55_abstraction/exercise_example/FreeWeight.java
@@ -0,0 +1,18 @@
+package day55_abstraction.exercise_example;
+
+public class FreeWeight extends Lifting{
+    @Override
+    public void perform() {
+
+    }
+
+    @Override
+    public int getCaloriesCount(int minutes) {
+        return 0;
+    }
+
+    @Override
+    public void endLift() {
+
+    }
+}
