5b37418783314764fd52edb1f0546fb68b845791
nancyshenouda <Shenouda.nancy@gmail.com>
2021-06-20 12:23:35 -0400

    Running class updated

--- src/day55_abstraction/exercise_example/Running.java
+++ src/day55_abstraction/exercise_example/Running.java
@@ -3,11 +3,13 @@ package day55_abstraction.exercise_example;
 public class Running extends Exercise{
     @Override
     public void perform() {
+        System.out.println("performing running exercise...");
 
     }
 
     @Override
     public int getCaloriesCount(int minutes) {
-        return 0;
+
+        return minutes*13;
     }
 }
