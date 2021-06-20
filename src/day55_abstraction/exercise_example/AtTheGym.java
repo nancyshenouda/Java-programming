9b3364dab519fa4fb9ff9f833113366f8bb0ee4b
nancyshenouda <Shenouda.nancy@gmail.com>
2021-06-20 12:53:57 -0400

    AtTheGym class with main method

---
+++ src/day55_abstraction/exercise_example/AtTheGym.java
@@ -0,0 +1,9 @@
+package day55_abstraction.exercise_example;
+
+public class AtTheGym {
+    public static void main(String[] args) {
+
+        //Exercise exercise = new Exercise(); // cannot create object/instantiate abstract class
+
+    }
+}
