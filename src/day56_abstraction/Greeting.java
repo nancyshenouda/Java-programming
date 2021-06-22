5c0bccc78cb845ff3836806d8c5d9b7600739afc
nancyshenouda <Shenouda.nancy@gmail.com>
2021-06-22 19:41:47 -0400

    day56_abstraction

---
+++ src/day56_abstraction/Greeting.java
@@ -0,0 +1,7 @@
+package day56_abstraction;
+
+public interface Greeting {
+// interface methods are already public abstract by default. We don't need to add them, it is added by compiler
+     void hi();
+     void bye();
+}
