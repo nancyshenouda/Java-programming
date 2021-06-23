f6e26a1fba08fd4461175162c1bf261271af2d90
nancyshenouda <Shenouda.nancy@gmail.com>
2021-06-22 22:03:12 -0400

    notes about interface

--- src/day56_abstraction/drivable/SelfDrivable.java
+++ src/day56_abstraction/drivable/SelfDrivable.java
@@ -2,4 +2,17 @@ package day56_abstraction.drivable;
 
 public interface SelfDrivable {
     void autoPiloting();
+
+    // void selfPark(); this causes error in sub classes, as it must be override abstract class
+
+    public default void selfPark() {
+        System.out.println("Performing self parking");
+        // if we add default keyword to the method to make it have implementation without breaking the code
+        // it is not an access modifier
+        // it is automatically public
+        //default is mendatory in interface c
+        // everything in interface is public
+
+
+    }
 }
